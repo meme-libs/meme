54940d385dfb1dbd23753ae945b9bf80de3295c6
yijie4188 <yijie4188@gmail.com>
2022-09-29 13:52:43 +0800

    chore(vite): pre build meme meta data to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from 'vite'
+import * as fs from 'fs'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function allMemes() {
+  return fs.readdirSync('./public/memes')
+}
 
 export default defineConfig({
   publicDir: 'public'
