b26df602199d39e39095bbf847e45196b9ba0253
yijie4188 <yijie4188@gmail.com>
2022-10-15 13:29:31 +0800

    feat(pages-index): a link style

--- pages/App.vue
+++ pages/App.vue
@@ -33,6 +33,13 @@ const isDark = useDark()
 const toggleDark = useToggle(isDark)
 </script>
 
+<style lang="scss">
+a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
+
 <style lang="scss" scoped>
 header {
   display: flex;
