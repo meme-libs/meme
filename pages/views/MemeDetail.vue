4705df3ecc506190b23c7e553fa789f664947ff7
yijie4188 <yijie4188@gmail.com>
2022-10-15 14:17:28 +0800

    feat(pages-index): meme detail page structure

--- pages/views/MemeDetail.vue
+++ pages/views/MemeDetail.vue
@@ -1,12 +1,20 @@
 <template>
-  <meme
-    :title="title"
-    :src-list="meme.srcList"
-  />
+  <div class="meme-with-autor">
+    <el-card>
+      <meme
+        :title="title"
+        :src-list="meme.srcList"
+      />
+    </el-card>
+    <div class="autor-meta">
+      some
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { computed } from 'vue'
+import { ElCard } from 'element-plus'
 import Meme from '@/components/meme.vue'
 
 const props = defineProps<{
@@ -23,4 +31,19 @@ const title = computed(() => meme.value?.title ?? '')
 </script>
 
 <style lang="scss" scoped>
+div.meme-with-autor {
+  display: flex;
+  :deep(div.el-card__body) > .meme {
+    > .el-carousel > .el-carousel__container {
+      height: 75vh;
+      .el-carousel__item img {
+        height: 100%;
+      }
+    }
+  }
+  > div.autor-meta {
+    padding: 10px;
+    min-width: 20vw;
+  }
+}
 </style>
