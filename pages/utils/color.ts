4941b6dbcbb1b732465169050585f2fb93458740
yijie4188 <yijie4188@gmail.com>
2022-10-16 13:45:21 +0800

    refactor(pages-index): remove color function to utils

---
+++ pages/utils/color.ts
@@ -0,0 +1,35 @@
+export function getRGB(hex: string): [number, number, number] {
+  const r = parseInt(hex.slice(0, 2), 16)
+  const g = parseInt(hex.slice(2, 4), 16)
+  const b = parseInt(hex.slice(4, 6), 16)
+  return [r, g, b]
+}
+
+export function getHSL(hex: string): [number, number, number] {
+  let [r, g, b] = getRGB(hex)
+  r /= 255
+  g /= 255
+  b /= 255
+  const max = Math.max(r, g, b), min = Math.min(r, g, b)
+  let h = 0, s = 0
+  const l = (max + min) / 2
+
+  if (max !== min) {
+    const d = max - min
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min)
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0)
+        break
+      case g:
+        h = (b - r) / d + 2
+        break
+      case b:
+        h = (r - g) / d + 4
+        break
+    }
+    h /= 6
+  }
+
+  return [Math.round(h * 360), Math.round(s * 100) | 0, Math.round(l * 100) | 0]
+}
